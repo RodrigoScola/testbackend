a9b2116d238380a635952efd96ee4d589628ae7c
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-09-03 19:55:54 -0300

    removed dist

--- dist/src/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Nomes_de_Tabelas = void 0;
-var Nomes_de_Tabelas;
-(function (Nomes_de_Tabelas) {
-    Nomes_de_Tabelas["FUNCIONARIOS"] = "funcionarios";
-})(Nomes_de_Tabelas = exports.Nomes_de_Tabelas || (exports.Nomes_de_Tabelas = {}));
