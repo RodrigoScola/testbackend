07916f84f915f14ca4168d2609c26cfcd7ff0561
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-09-03 19:45:40 -0300

    SQL

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,13 @@ import express from "express";
 // const PORT = process.env.port || 8080;
 
 // // bas
-// export const SQLcliente = mysql.createConnection({
-//   host: process.env.AWS_RDS_HOST,
-//   port: parseInt(process.env.AWS_RDS_PORT || "3306") || 3306,
-//   password: process.env.AWS_RDS_PASSWORD,
-//   database: process.env.AWS_RDS_DATABASE_NAME,
-//   user: process.env.AWS_RDS_USERNAME,
-// });
+export const SQLcliente = mysql.createConnection({
+  host: process.env.AWS_RDS_HOST,
+  port: parseInt(process.env.AWS_RDS_PORT || "3306") || 3306,
+  password: process.env.AWS_RDS_PASSWORD,
+  database: process.env.AWS_RDS_DATABASE_NAME,
+  user: process.env.AWS_RDS_USERNAME,
+});
 
 const app = express();
 // const rota = Router();
@@ -38,20 +38,20 @@ app.get("/", (req, res) => {
 });
 
 app.listen(8080, async () => {
-  // SQLcliente.connect((err) => {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-  //   console.log("> Conectado na Database");
-  //   try {
-  //     SQLcliente.query(
-  //       `create table if not exists funcionarios ( id text not null, email varchar(50) not null, nome varchar(50) not null, empresa varchar(50) not null, telefone varchar(15) not null, created_at datetime default now())`
-  //     );
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // });
+  SQLcliente.connect((err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("> Conectado na Database");
+    try {
+      SQLcliente.query(
+        `create table if not exists funcionarios ( id text not null, email varchar(50) not null, nome varchar(50) not null, empresa varchar(50) not null, telefone varchar(15) not null, created_at datetime default now())`
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
   console.log(`Servidor funcionando no port ${8080}`);
 });
