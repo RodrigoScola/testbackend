8d4511e42105b09aeb260220886e0f15e2e86c72
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-09-04 09:56:47 -0300

    traducao de testes

--- __tests__/tests.test.ts
+++ __tests__/tests.test.ts
@@ -1,74 +1,74 @@
-// import { ProcessadorDeMensagens } from "../src/ProcessadorMensagens";
-// import { User } from "../src/User";
-// import { NovaMensagemInfo } from "../src/types";
-// import { Validacao } from "../src/utils/Validacao";
+import { ProcessadorDeMensagens } from "../src/ProcessadorMensagens";
+import { User } from "../src/User";
+import { NovaMensagemInfo } from "../src/types";
+import { Validacao } from "../src/utils/Validacao";
 
-// describe("Username From Email Tests", () => {
-//   it("Should return a string from UserNameFromEmail", () => {
-//     expect(typeof User.userNameFromEmail("Hayden@althea.biz")).toEqual(
-//       "string"
-//     );
-//   });
-//   it("The user_name should be Hayden", () => {
-//     expect(User.userNameFromEmail("Hayden@althea.biz")).toEqual("Hayden");
-//   });
+describe("Testes Username no email", () => {
+  it("Deve retornar uma string da função", () => {
+    expect(typeof User.userNameFromEmail("Hayden@althea.biz")).toEqual(
+      "string"
+    );
+  });
+  it("O user_name deve ser Hayden", () => {
+    expect(User.userNameFromEmail("Hayden@althea.biz")).toEqual("Hayden");
+  });
 
-//   it("The user_name should be Presley Mueller", () => {
-//     expect(User.userNameFromEmail("Presley.Mueller@myrl.com")).toEqual(
-//       "Presley Mueller"
-//     );
-//   });
-//   it("The user_name should be Mallory Kunze", () => {
-//     expect(User.userNameFromEmail("Mallory_Kunze@marie.org")).toEqual(
-//       "Mallory Kunze"
-//     );
-//   });
-//   it("The user_name should be John Green Mackeys", () => {
-//     expect(User.userNameFromEmail("John_Green.Mackeys@marie.org")).toEqual(
-//       "John Green Mackeys"
-//     );
-//   });
-// });
+  it("O user_name deve ser Presley Mueller", () => {
+    expect(User.userNameFromEmail("Presley.Mueller@myrl.com")).toEqual(
+      "Presley Mueller"
+    );
+  });
+  it("O user_name deve ser Mallory Kunze", () => {
+    expect(User.userNameFromEmail("Mallory_Kunze@marie.org")).toEqual(
+      "Mallory Kunze"
+    );
+  });
+  it("O user_name deve ser John Green Mackeys", () => {
+    expect(User.userNameFromEmail("John_Green.Mackeys@marie.org")).toEqual(
+      "John Green Mackeys"
+    );
+  });
+});
 
-// describe("Message Processor test", () => {
-//   test("Should add usernames to objects", () => {
-//     const base_messages = [
-//       { id: 1, email: "Eliseo@gardner.biz" },
-//       { id: 2, email: "Jayne_Kuhic@sydney.com" },
-//       { id: 3, email: "Nikita@garfield.biz" },
-//     ];
-//     const with_usernames = [
-//       { id: 1, email: "Eliseo@gardner.biz", user_name: "Eliseo" },
-//       { id: 2, email: "Jayne_Kuhic@sydney.com", user_name: "Jayne Kuhic" },
-//       { id: 3, email: "Nikita@garfield.biz", user_name: "Nikita" },
-//     ];
-//     const newMessages = ProcessadorDeMensagens.AdicionarUsernames(
-//       base_messages as NovaMensagemInfo[]
-//     );
-//     expect(newMessages).toStrictEqual(with_usernames);
-//   });
-// });
+describe("Testes de Processador de mensagens", () => {
+  test("Should add usernames to objects", () => {
+    const mensagens_base = [
+      { id: 1, email: "Eliseo@gardner.biz" },
+      { id: 2, email: "Jayne_Kuhic@sydney.com" },
+      { id: 3, email: "Nikita@garfield.biz" },
+    ];
+    const com_usernames = [
+      { id: 1, email: "Eliseo@gardner.biz", user_name: "Eliseo" },
+      { id: 2, email: "Jayne_Kuhic@sydney.com", user_name: "Jayne Kuhic" },
+      { id: 3, email: "Nikita@garfield.biz", user_name: "Nikita" },
+    ];
+    const newMessages = ProcessadorDeMensagens.AdicionarUsernames(
+      mensagens_base as NovaMensagemInfo[]
+    );
+    expect(newMessages).toStrictEqual(com_usernames);
+  });
+});
 
-// describe("Validation Tests", () => {
-//   describe("Valid Email Tests", () => {
-//     it("Should return true when receiving a valid email", async () => {
-//       expect(Validacao.emailValido("rodrigo.sgarabotto.scola@gmail.com")).toBe(
-//         true
-//       );
-//     });
-//     it("Should return false when receiving an invalid email", async () => {
-//       expect(Validacao.emailValido("rodrigo.sgarabotto.scola.gmail.com")).toBe(
-//         false
-//       );
-//     });
-//   });
-//   describe("Valid Phone Number Tests", () => {
-//     it("Should return true when receiving a valid Phone Number", async () => {
-//       expect(Validacao.telefoneValido("54 991982471")).toBe(true);
-//       expect(Validacao.telefoneValido("991982471")).toBe(true);
-//     });
-//     it("Should return false when receiving an invalid phone number", async () => {
-//       expect(Validacao.telefoneValido("55 54 991982471")).toBe(true);
-//     });
-//   });
-// });
+describe("Testes de validação", () => {
+  describe("Testes de emails válidos", () => {
+    it("Deve retornar verdadeiro quando o input for um email válido", () => {
+      expect(Validacao.emailValido("rodrigo.sgarabotto.scola@gmail.com")).toBe(
+        true
+      );
+    });
+    it("Deve retornar falso quando o input for um email inválido", () => {
+      expect(Validacao.emailValido("rodrigo.sgarabotto.scola.gmail.com")).toBe(
+        false
+      );
+    });
+  });
+  describe("Valid Phone Number Tests", () => {
+    it("Deve retornar verdadeiro quando receber um telefone válido", () => {
+      expect(Validacao.telefoneValido("54 991982471")).toBe(true);
+      expect(Validacao.telefoneValido("991982471")).toBe(true);
+    });
+    it("Deve retornar falso quando receber um telefone inválido", () => {
+      expect(Validacao.telefoneValido("55 54 991982471")).toBe(false);
+    });
+  });
+});
